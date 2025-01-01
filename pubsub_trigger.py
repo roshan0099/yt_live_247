b1e20613fa7886bf207d0e0c0eea543cd28fc0ef
roshan0099 <roshan09ka@gmail.com>
2025-01-01 20:32:48 +0530

    adding pubsub trigger

---
+++ pubsub_trigger.py
@@ -0,0 +1,37 @@
+import os
+from google.cloud import pubsub_v1
+import json
+
+project_id = "linen-patrol-428513-u3"
+sub_id = "gcs-alert-sub"
+
+print("started ---- listening the surrounding")
+subscriber = pubsub_v1.SubscriberClient()
+subscripion_path = subscriber.subscription_path(project_id,sub_id)
+
+def callback(msg):
+    
+    try :
+        
+        print(f"Received message: {msg.data.decode('utf-8')}")
+        print(f"==> got here {msg} ")
+
+        msg.ack()
+
+    except Exception as e :
+        print("smth went wrong :  => ",e)
+        msg.nack()
+
+
+subscriber_pull = subscriber.subscribe(subscripion_path,callback)
+
+
+try :
+
+    #continously listenig 
+    subscriber_pull.result()
+
+except KeyboardInterrupt :
+    subscriber_pull.cancel()
+
+    print("---done")
